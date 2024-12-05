528e9958e93704bdafe4dd682e4187ea1d323532
Nick MacKenzie <contact.campreviews@gmail.com>
2024-12-04 22:41:35 -0500

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,22 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
   base: '/',
- 
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        privacy: resolve(__dirname, 'privacy.html')
+      }
+    },
+    cssCodeSplit: false,
+    outDir: 'dist',
+    assetsDir: 'assets'
+  },
+  css: {
+    preprocessorOptions: {
+      // Add any preprocessor options here if you're using SASS/LESS/etc
+    }
+  }
 })
